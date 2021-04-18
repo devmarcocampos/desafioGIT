065d081acbc69a936e6fd9de5ddc0afbf90e3aed
Marco <campos.marcoa@yahoo.com.br>
2021-04-18 20:17:36 -0300

    populando tela de detalhes

--- app/src/main/java/com/example/desafiogitapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/desafiogitapp/ui/details/DetailsFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import com.example.desafiogitapp.R
 import com.example.desafiogitapp.data.model.Items
+import com.squareup.picasso.Picasso
 
 class DetailsFragment(
     private val repositorie: Items
@@ -25,6 +28,35 @@ class DetailsFragment(
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        Toast.makeText(activity, "Detalhes Fragment -" + repositorie.name, Toast.LENGTH_LONG).show()
+//        Toast.makeText(activity, "Detalhes Fragment -" + repositorie.name, Toast.LENGTH_LONG).show()
+
+        showDetails()
+    }
+
+    private fun showDetails() {
+        val ownerImageView = view?.findViewById<ImageView>(R.id.ownerImageView)
+        val ownerNameTextView = view?.findViewById<TextView>(R.id.ownerNameTextView)
+        val repositorieNameTextView = view?.findViewById<TextView>(R.id.repositorieNameTextView)
+        val repositorieStarsTextView = view?.findViewById<TextView>(R.id.repositorieStarsTextView)
+        val repositorieForksTextView = view?.findViewById<TextView>(R.id.repositorieForksTextView)
+
+        ownerNameTextView?.text = repositorie.owner.login
+        repositorieNameTextView?.text = repositorie.name
+        repositorieStarsTextView?.text = repositorie.stargazersCount.toString()
+        repositorieForksTextView?.text = repositorie.forks.toString()
+
+        val picasso = activity?.let {
+            Picasso.Builder(it).listener { _, _, exception ->
+            exception?.printStackTrace()
+            println("Picasso loading failed : ${exception?.message}")
+            ownerImageView?.setImageResource(R.drawable.ic_launcher_background)
+        }.build()
+        }
+
+        picasso?.let { pic ->
+            pic.load(repositorie.owner.avatar_url)
+                    .fit().centerCrop()
+                    .into(ownerImageView)
+        }
     }
 }
